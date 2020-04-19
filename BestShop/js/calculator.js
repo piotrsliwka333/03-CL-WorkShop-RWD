5da8728d10971829b38466978231a5cc881aeb80
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-04-19 20:54:14 +0200

    Created Footer and Visual Calculator

---
+++ BestShop/js/calculator.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function () {
+	const productQuantity = document.querySelector('.products-quantity');
+	const estimatedOrders = document.querySelector('.estimated-orders-month');
+	const ordersPlaceToInner = document.querySelector('.orders-quantity');
+
+})
