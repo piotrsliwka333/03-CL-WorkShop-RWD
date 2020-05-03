a473af14cc33f0211a29b17386e3669357c91bb8
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-05-03 17:46:15 +0200

    Fixed bug with visible title after click in mobile mode toogle resize to desktop and back to mobile

--- BestShop/js/menu.js
+++ BestShop/js/menu.js
@@ -7,6 +7,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		const elemToHide = document.querySelector('.jumbotron-box__title');
 		elemToHide.classList.toggle('visible');
 		elementToShow.classList.toggle('hide');
+		const mobile = window.matchMedia("(min-width: 1000px)");
+		mobile.addListener( function(mobile) {
+			if (mobile.matches) {
+				elemToHide.classList.remove('visible');
+				console.log(mobile.matches);
+				console.log(elementToShow.className)
+			} else if (mobile.matches === false && elementToShow.className.indexOf("hide") === -1) {
+				console.log("hass");
+				elemToHide.classList.add('visible');
+			}
+		});
 
 	})
 
